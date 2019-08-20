0946c29c6667e13bbd190847495e67156b7bf47c
Matias <matias.spinelli@wolox.com.ar>
2019-08-20 19:48:10 -0300

    Initial Commit

---
+++ WolmoView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WolmoView
+//
+//  Created by Matías Spinelli on 20/08/2019.
+//  Copyright © 2019 Matías Spinelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
